c025f001b37e6db037f97d1b91c94a1b247e3105
ranim <ranimabdellaoui258@gmail.com>
2026-01-05 16:49:23 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PlayersList from "./PlayersList";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", margin: "20px" }}>
+        FIFA Player Cards
+      </h1>
+      <PlayersList />
+    </div>
+  );
+}
+
+export default App;
+
